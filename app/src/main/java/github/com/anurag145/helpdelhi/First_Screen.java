8f0da92e8757792948a38d4e67510e57c58921c4
anurag145 <anurag145@gmail.com>
2016-11-05 21:32:54 +0530

    initial push

---
+++ app/src/main/java/github/com/anurag145/helpdelhi/First_Screen.java
@@ -0,0 +1,13 @@
+package github.com.anurag145.helpdelhi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class First_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first__screen);
+    }
+}
