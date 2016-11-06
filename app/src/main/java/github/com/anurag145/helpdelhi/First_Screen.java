1ae781f5a0281abdf8de7a493a64f103e843a8fb
anurag145 <anurag145@gmail.com>
2016-11-06 18:59:03 +0530

    data accepted from server now

--- app/src/main/java/github/com/anurag145/helpdelhi/First_Screen.java
+++ app/src/main/java/github/com/anurag145/helpdelhi/First_Screen.java
@@ -1,5 +1,6 @@
 package github.com.anurag145.helpdelhi;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -9,6 +10,14 @@ import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class First_Screen extends AppCompatActivity {
  private RecyclerView Recyclerview;
@@ -19,15 +28,30 @@ public class First_Screen extends AppCompatActivity {
         setContentView(R.layout.activity_first__screen);
         if (Build.VERSION.SDK_INT >= 23) {
 
-            if (ActivityCompat.checkSelfPermission(First_Screen.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-            {  requestpermisions();
-            }else
-            {
-                Intent intent = new Intent(this,Upload.class);
-                startActivity(intent);
+            if (ActivityCompat.checkSelfPermission(First_Screen.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestpermisions();
+            } else {
+
             }
         }
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,"http://sponsorhub.xyz/api/v1/products",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        Log.d("hello",s);
 
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                   Log.d("error : ",volleyError.toString());
+
+                    }
+                });
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+                queue.add(stringRequest);
 
     }
     public void requestpermisions() {
